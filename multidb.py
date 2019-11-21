46caf5d08479b690f08ec70f54c6684050a894c5
ChantYuCN <yu.chengde@99cloud.net>
2019-11-21 04:58:18 -0500

    multiple database

---
+++ multidb.py
@@ -0,0 +1,16 @@
+## need pip install sqlalchemy-multidb 
+
+from sqlalchemy_multidb import DatabaseManager
+from one2many import Man
+from one2one import Parent
+
+
+db = DatabaseManager()
+db.add_database('test1to1','mysql+pymysql://root@localhost:3306/test1to1')
+db.add_database('test1toM','mysql+pymysql://root@localhost:3306/test1toM')
+
+with db.session('test1toM') as s1:
+    h1 = s1.query(Man)
+
+with db.session('test1to1') as s2:
+    h2 = s2.query(Parent)
